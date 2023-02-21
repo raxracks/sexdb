26bd4fc943d873555af2b26b47e71a0014b3de58
raxracks <multisniperism@gmail.com>
2023-02-22 12:05:28 +1300

    add id system

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ use std::net::TcpListener;
 use std::thread::spawn;
 use tungstenite::{accept, Message};
 
+static ADDRESS: &str = "127.0.0.1:9001";
+
 fn main() {
-    let server = TcpListener::bind("127.0.0.1:9001").unwrap();
+    let server = TcpListener::bind(ADDRESS).unwrap();
+    println!("Running on ws://{}", ADDRESS);
     for stream in server.incoming() {
         let mut data = HashMap::<String, String>::new();
         spawn(move || {
@@ -14,34 +17,32 @@ fn main() {
 
                 if msg.is_binary() || msg.is_text() {
                     let split = msg.to_text().unwrap().split(" ").collect::<Vec<&str>>();
-                    let instruction = split[0];
-                    let result: String;
+                    let id = split[0].to_string();
+                    let instruction = split[1].to_uppercase();
 
-                    match instruction {
-                        "SET" => {
-                            let key = split[1].to_string();
-                            let value = split[2..].join(" ");
-                            
-                            data.insert(key, value);
-                            result = "SUCCESS".to_string();
-                            ()
-                        }
-                        "GET" => {
-                            let key = split[1].to_string();
-                            
-                            result = data
-                                .get(&key)
-                                .unwrap_or(&"ERROR: Key not in store".to_string())
-                                .to_string();
-                            ()
-                        }
-                        _ => {
-                            result = "ERROR: Invalid instruction".to_string();
-                            ()
-                        }
-                    }
+                    websocket
+                        .write_message(Message::Text(match instruction.as_str() {
+                            "SET" => {
+                                let key = split[2].to_string();
+                                let value = split[3..].join(" ");
 
-                    websocket.write_message(Message::Text(result)).unwrap();
+                                data.insert(key, value);
+                                format!("{} SUCCESS", id)
+                            }
+                            "GET" => {
+                                let key = split[2].to_string();
+
+                                format!(
+                                    "{} {}",
+                                    id,
+                                    data.get(&key)
+                                        .unwrap_or(&"ERROR: Key not in store".to_string())
+                                        .to_string()
+                                )
+                            }
+                            _ => format!("{} ERROR: Invalid instruction", id),
+                        }))
+                        .unwrap();
                 }
             }
         });
